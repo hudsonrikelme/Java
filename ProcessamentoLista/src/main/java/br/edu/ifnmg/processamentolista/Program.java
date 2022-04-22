63d8f7746a6a6ad43cc285fac1f4c93d37d59f37
Rikelme <Rikelme@PC_Rikelme>
2022-04-22 11:11:34 -0300

    Teste GitHub

--- ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/Program.java
+++ ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/Program.java
@@ -8,5 +8,7 @@ package br.edu.ifnmg.processamentolista;
 public class Program {
     public static void main(String[] args) {
         
+        System.out.println(">> Hello World!<<");
+        
     }
 }
