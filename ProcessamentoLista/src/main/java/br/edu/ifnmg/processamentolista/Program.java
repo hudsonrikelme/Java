9c18ba283984531324cd926be9a2c10b79fe1736
Rikelme <Rikelme@PC_Rikelme>
2022-04-27 20:30:35 -0300

    Atividade Finalizada, depois de realizar todos os 5 testes.

--- ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/Program.java
+++ ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/Program.java
@@ -14,13 +14,13 @@ public class Program {
         List<Double> outs = new ArrayList<Double>();
 
 //     --==Teste 1 ==--
-//        ins.add(1.0);
-//        ins.add(3.0);
-//        ins.add(5.0);
-//
-//        outs.add(1.0);
-//        outs.add(3.0);
-//        outs.add(5.0);
+        ins.add(1.0);
+        ins.add(3.0);
+        ins.add(5.0);
+
+        outs.add(1.0);
+        outs.add(3.0);
+        outs.add(5.0);
         
         
 //     --==Teste 2 ==--        
@@ -54,43 +54,43 @@ public class Program {
 
 
 //        --==Teste 5 ==--
-          ins.add(2.900000095);
-          ins.add(6.699999809);
-          ins.add(4.900000095);
-          ins.add(7.900000095);
-          ins.add(9.800000191);
-          ins.add(6.900000095);
-          ins.add(6.099999905);
-          ins.add(6.199999809);
-          ins.add(6.000000000);
-          ins.add(5.099999905);
-          ins.add(4.699999809);
-          ins.add(4.400000095);
-          ins.add(5.800000191);
-          
-          outs.add(4.000000000);
-          outs.add(7.400000095);
-          outs.add(5.000000000);
-          outs.add(7.199999809);
-          outs.add(7.900000095);
-          outs.add(6.099999905);
-          outs.add(6.000000000);
-          outs.add(5.800000191);
-          outs.add(5.199999809);
-          outs.add(4.199999809);
-          outs.add(4.000000000);
-          outs.add(4.400000095);
-          outs.add(5.199999809);
-          
+//          ins.add(2.900000095);
+//          ins.add(6.699999809);
+//          ins.add(4.900000095);
+//          ins.add(7.900000095);
+//          ins.add(9.800000191);
+//          ins.add(6.900000095);
+//          ins.add(6.099999905);
+//          ins.add(6.199999809);
+//          ins.add(6.000000000);
+//          ins.add(5.099999905);
+//          ins.add(4.699999809);
+//          ins.add(4.400000095);
+//          ins.add(5.800000191);
+//          
+//          outs.add(4.000000000);
+//          outs.add(7.400000095);
+//          outs.add(5.000000000);
+//          outs.add(7.199999809);
+//          outs.add(7.900000095);
+//          outs.add(6.099999905);
+//          outs.add(6.000000000);
+//          outs.add(5.800000191);
+//          outs.add(5.199999809);
+//          outs.add(4.199999809);
+//          outs.add(4.000000000);
+//          outs.add(4.400000095);
+//          outs.add(5.199999809);
+//          
 
-//        Dupla teste1;
-//
-//        
-//        teste1 = RegressaoLinear.metodo(ins, outs);
-//
-//        System.out.println("W1: " + String.format("%.3f", teste1.getX())
-//                + "\nW0: " + String.format("%.3f", teste1.getY())
-//                + "\n");
+        Dupla teste1;
+
+        
+        teste1 = RegressaoLinear.metodo(ins, outs);
+
+        System.out.println("W1: " + String.format("%.3f", teste1.getX())
+                + "\nW0: " + String.format("%.3f", teste1.getY())
+                + "\n");
 //        
         
 //        --==Teste 2 ==--
@@ -130,13 +130,13 @@ public class Program {
         
 //        --==Teste 5 ==--
 
-        Dupla teste5;
-        
-        
-        teste5 = RegressaoLinear.metodo2(ins, outs,2000, 0.025);
-        System.out.println("W1: " + String.format("%.3f", teste5.getX())
-                + "\nW0: " + String.format("%.3f", teste5.getY())
-                + "\n");
+//        Dupla teste5;
+//        
+//        
+//        teste5 = RegressaoLinear.metodo2(ins, outs,2000, 0.025);
+//        System.out.println("W1: " + String.format("%.3f", teste5.getX())
+//                + "\nW0: " + String.format("%.3f", teste5.getY())
+//                + "\n");
         
         
         
