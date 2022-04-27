9cbdc8e89173e4335265a2ec0658a9b0164525c0
Rikelme <Rikelme@PC_Rikelme>
2022-04-27 13:30:20 -0300

    Feito até o teste 3 com seus devidos métodos

--- ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/Program.java
+++ ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/Program.java
@@ -1,4 +1,3 @@
-
 package br.edu.ifnmg.processamentolista;
 
 import java.util.ArrayList;
@@ -9,25 +8,92 @@ import java.util.List;
  * @author Rikelme
  */
 public class Program {
+
     public static void main(String[] args) {
-        List<Double> ins =  new ArrayList<Double>();
+        List<Double> ins = new ArrayList<Double>();
         List<Double> outs = new ArrayList<Double>();
+
+//     --==Teste 1 ==--
+//        ins.add(1.0);
+//        ins.add(3.0);
+//        ins.add(5.0);
+//
+//        outs.add(1.0);
+//        outs.add(3.0);
+//        outs.add(5.0);
         
+        
+//     --==Teste 2 ==--        
+//        ins.add(1.0);
+//        ins.add(3.0);
+//        ins.add(5.0);
+//        
+//        outs.add(2.0);
+//        outs.add(4.0);
+//        outs.add(6.0);
+        
+
+//     --==Teste 3 ==--
         ins.add(1.0);
         ins.add(3.0);
         ins.add(5.0);
         
-        outs.add(1.0);
         outs.add(3.0);
-        outs.add(5.0);
-        
-        
-        Dupla teste;
-        
-        teste = RegressaoLinear.metodo(ins, outs);
+        outs.add(7.0);
+        outs.add(11.0); 
 
-        System.out.println("W1: " + String.format("%.3f", teste.getX())
-                + "\nW0: " + String.format("%.3f", teste.getY()));
+        
+//     --==Teste 4 ==--
+        ins.add(1.0);
+        ins.add(3.0);
+        ins.add(5.0);
+        
+        outs.add(4.5);
+        outs.add(12.5);
+        outs.add(20.5); 
+
+//        --==Teste 1 ==--
+
+//        Dupla teste1;
+//
+//        
+//        teste1 = RegressaoLinear.metodo(ins, outs);
+//
+//        System.out.println("W1: " + String.format("%.3f", teste1.getX())
+//                + "\nW0: " + String.format("%.3f", teste1.getY())
+//                + "\n");
+//        
+        
+//        --==Teste 2 ==--
+
+//        Dupla teste2;
+//
+//        teste2 = RegressaoLinear.metodo1(ins, outs, 2000);
+//        
+//        System.out.println("W1: " + String.format("%.3f", teste2.getX())
+//                + "\nW0: " + String.format("%.3f", teste2.getY())
+//                + "\n");
+        
+
+
+//        --==Teste 3 ==--
+
+//        Dupla teste3;
+//        
+//        
+//         teste3 = RegressaoLinear.metodo1(ins, outs, 2000);
+//        
+//        System.out.println("W1: " + String.format("%.3f", teste3.getX())
+//                + "\nW0: " + String.format("%.3f", teste3.getY())
+//                + "\n");
+
+
+//        --==Teste 4 ==--
+
+
+        
+        
+        
         
     }
 }
--- ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/RegressaoLinear.java
+++ ProcessamentoLista/src/main/java/br/edu/ifnmg/processamentolista/RegressaoLinear.java
@@ -7,40 +7,82 @@ import java.util.List;
  * @author Rikelme
  */
 public class RegressaoLinear {
-    
+
     private static Double w0 = 0.0;
     private static Double w1 = 0.0;
     private static Double p;
     private static Double dif;
     private static Integer e = 2000;
     private static Double lr = 0.025;
-    
-    private static Dupla<Double, Double> imp = new Dupla<>(w0,w1);
-    
-    
-    public static Dupla metodo(List<Double> ins, List <Double> outs){
-        for(int i = 1; i<=2000;i++){
-    
-        if((i%10)==0){
-        
-            System.out.printf("%d %.3f %.3f \n", i,w1,w0);
-            
+
+    private static Dupla<Double, Double> imp = new Dupla<>(w0, w1);
+
+    public static Dupla metodo(List<Double> ins, List<Double> outs) {
+        for (int i = 1; i <= 2000; i++) {
+
+            if ((i % 10) == 0) {
+
+                System.out.println("i: " + i
+                        + "  W1: " + String.format("%.3f", w1)
+                        + "  W0: " + String.format("%.3f", w0));
+
+            }
+            for (int j = 0; j < ins.size(); j++) {
+
+                p = w1 * ins.get(j) + w0;
+                dif = outs.get(j) - p;
+                w0 += dif * lr;
+                w1 += dif * lr * ins.get(j);
+
+            }
+
         }
-        for(int j=0; j<ins.size();j++){
-        
-            p = w1 * ins.get(j) + w0;
-            dif  = outs.get(j) - p;
-            w0 += dif * lr;
-            w1 += dif * lr * ins.get(j);
-        
-        }
-    
-    }
         imp.setX(w1);
         imp.setY(w0);
 
-    return imp;
-        
+        return imp;
+
     }
-    
+
+    public static Dupla metodo1(List<Double> ins, List<Double> outs, int e) {
+        for (int i = 1; i <= e; i++) {
+            if ((i % 10) == 0) {
+
+                System.out.println("i: " + i
+                        + "  W1: " + String.format("%.3f", w1)
+                        + "  W0: " + String.format("%.3f", w0));
+            }
+            for (int j = 0; j < ins.size(); j++) {
+                p = w1 * ins.get(j) + w0;
+                dif = outs.get(j) - p;
+                w0 += dif * lr;
+                w1 += dif * lr * ins.get(j);
+            }
+        }
+
+        imp.setX(w1);
+        imp.setY(w0);
+        return imp;
+    }
+
+    public static Dupla metodo2(List<Double> ins, List<Double> outs, int e, Double lr) {
+        for (int i = 1; i <= e; i++) {
+            if ((i % 10) == 0) {
+                System.out.println("i: " + i
+                        + "  W1: " + String.format("%.3f", w1)
+                        + "  W0: " + String.format("%.3f", w0));
+            }
+            for (int j = 0; j < ins.size(); j++) {
+                p = w1 * ins.get(j) + w0;
+                dif = outs.get(j) - p;
+                w0 += dif * lr;
+                w1 += dif * lr * ins.get(j);
+            }
+        }
+
+        imp.setX(w1);
+        imp.setY(w0);
+        return imp;
+    }
+
 }
